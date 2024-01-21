56ca17b227428bf8d6288c627222a471221079ae
SMIT GenAI <154240932+saylani-genai@users.noreply.github.com>
2024-01-21 20:49:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+console.log("Hello Pakistan") // Console the command
+
+// Variable --> 
+var a = "Hammad Sheikh" // String
+var b = 12 // Number
+var bool = true
+console.log(a)
+
+// Show in the web Page
+
+// Data Types
+
+// String 
+// var str = "Pakistan"
+// var num = 120
+// var bool1 = true
+// var bool2 = false
+// var arr = [12, "Hammad", true]
+
+
+// console.log(arr)
+// console.table(bool2)
+
+// confirm("Are your sure to Proceed")
+// var inp = prompt("Enter the Number: ")
+
+// document.write(`${inp} x 1 =`, inp * 1 + "<br>") // For writing the Dynamic value we use "$" with bacticks
+// document.write(`${inp} x 2 =`, inp * 2 + "<br>")
+// document.write(`${inp} x 3 =`, inp * 3 + "<br>")
+// document.write(`${inp} x 4 =`, inp * 4 + "<br>")
+// document.write(`${inp} x 5 =`, inp * 5 + "<br>")
+// document.write(`${inp} x 6 =`, inp * 6 + "<br>")
+// document.write(`${inp} x 7 =`, inp * 7 + "<br>")
+// document.write(`${inp} x 8 =`, inp * 8 + "<br>")
+// document.write(`${inp} x 9 =`, inp * 9 + "<br>")
+// document.write(`${inp} x 10 =`, inp * 10 + "<br>")
+
+
+// Arthematic operator
+var sum1 = 10
+var sum2 = 20
+var add = sum1 + sum2
+var sub = sum1 - sum2
+var mul = sum1 * sum2
+var div = sum1 / sum2
+var mod = sum1 % sum2
+
+
+console.log("The Result of Addition is : " + add)
+console.log( "The Result of Subtraction is : " +  sub)
+console.log( "The Result of Multiplication is : " +  mul)
+console.log("The Result of Divison is : " +  div)
+console.log("The Result of Modulation is : " +  mod)
+
+// constant
+var a1 = "Jack"
+var a1 = "Karachi"
+console.log(a1)
+
+const data1 = "Computer Science" // Const value can not be re declear 
+console.log(data1)
+// const data1 = "react"
+
+let x = 3;
+const y = --x;
+// ++, --, ===, !
+
+const fls = !true
+console.log(fls)
+
+console.log(y)
+// Expected output: "x:4, y:3"
+
+
+
+
+// Expected output: "a:4, b:4"
